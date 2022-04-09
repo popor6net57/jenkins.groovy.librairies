6452160d51687344569bc5a06d25bff02e0926e8
popor6net57 <dev.florian.maliverney@gmail.com>
2022-04-09 17:29:38 +0200

    wdwdwd

--- test/src/cs/jenkins/demo/Animal.groovy
+++ test/src/cs/jenkins/demo/Animal.groovy
@@ -2,8 +2,9 @@ package cs.jenkins.demo;
 
 class Animal {
    String name;
+   String kind;
 
-   public Animal(String name){
+   public Animal(String name, String kind == "dog"){
     this.name = name;
    }
 
